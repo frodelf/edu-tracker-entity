50137d1a9026d9bbad60a9004023f225322a9655
Denis Derkach <derkach.denis@lll.kpi.ua>
2024-08-20 17:33:03 +0300

    Add default value for user's field

--- src/main/java/ua/kpi/edutrackerentity/entity/User.java
+++ src/main/java/ua/kpi/edutrackerentity/entity/User.java
@@ -10,19 +10,19 @@ public abstract class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(length = 100)
-    private String lastName;
+    private String lastName="";
     @Column(length = 100)
-    private String name;
+    private String name="";
     @Column(length = 100)
-    private String middleName;
+    private String middleName="";
     @Column(length = 100)
     private String image;
     @Column(length = 100, unique = true)
-    private String phone;
+    private String phone="";
     @Column(length = 100, unique = true)
-    private String email;
+    private String email="";
     @Column(length = 100, unique = true)
-    private String telegram;
+    private String telegram="";
     @Column(length = 100)
     private String password;
 }
